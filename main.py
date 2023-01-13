1e06534089a178c8e59819f61de04730fcb5c422
nga-office <nga.office19@gmail.com>
2023-01-13 13:15:37 +0000

    ADD: BUILD THE ANNULATIONS REGEX

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import copy
+import csv
+import io
 import os
 import pathlib
 import re
-import sys
 from datetime import *
 from typing import Dict, List, Tuple
 
@@ -14,27 +15,56 @@ from dateutil.parser import *
 from dateutil.relativedelta import *
 from dateutil.rrule import *
 
-from constants import LOG_PATH_DIR, PRELIS_PATH_DIR
+from constants import ANNULATION_REGEX, LOG_PATH_DIR, VIREMENT_REGEX
 
 
 class PrelisFactory:
-    def readFile(self, filename: str) -> List[Tuple]:
+    def __init__(self, filename: str):
+        self._filename = filename
+        self._lines = None
+        self._recipe = None
+
+    @property
+    def filename(self):
+        return self._filename
+
+    @filename.setter
+    def filename(self, new_filename: str):
+        self._filename = new_filename
+
+    @property
+    def recipe(self):
+        return self._recipe
+
+    @recipe.setter
+    def recipe(self, new_recipe: List):
+        self._recipe = new_recipe
+
+    @property
+    def lines(self):
+        return self._lines
+
+    @lines.setter
+    def lines(self, new_lines: List):
+        self._lines = new_lines
+
+    def readFile(self) -> List[Tuple]:
         filepath = pathlib.Path(__file__).parent
-        filepath = filepath / f'{PRELIS_PATH_DIR}/{filename}'
+        filepath = filepath / f'{PRELIS_PATH_DIR}/{self.filename}'
         with open(filepath) as file:
-            lines = file.readlines()
-            for index, line in enumerate(lines):
+            self.lines = file.readlines()
+            for index, line in enumerate(self.lines):
                 line = line.replace('\n', '')
                 line = line.replace('  ', '')
-                lines[index] = tuple(line.split(' '))
+                self.lines[index] = tuple(line.split(' '))
             file.close()
-        return lines
+        return self.lines
 
-    def extractDate(self, datas: list) -> List[Dict]:
-        recipient = copy.deepcopy(datas)
-        for index, (pan, toChange) in enumerate(recipient):
+    def extractDate(self) -> List[Dict]:
+        self.recipe = copy.deepcopy(self.lines)
+        for index, (pan, toChange) in enumerate(self.recipe):
             date = pan[2:12]
-            recipient[index] = {
+            self.recipe[index] = {
                 'ID': index,
                 'pan': pan,
                 'toChange': toChange,
@@ -48,8 +78,8 @@ class PrelisFactory:
                     'minutes': date[8:10],
                 },
             }
-            date = recipient[index].get('dateObject')
-            recipient[index].update({
+            date = self.recipe[index].get('dateObject')
+            self.recipe[index].update({
                 'datetime': datetime(
                     int(f"20{date.get('years')}"),
                     int(date.get('months')),
@@ -58,7 +88,7 @@ class PrelisFactory:
                     int(date.get('minutes')),
                 )
             })
-            recipient[index].update({
+            self.recipe[index].update({
                 'isoDatetime': datetime(
                     int(f"20{date.get('years')}"),
                     int(date.get('months')),
@@ -67,38 +97,63 @@ class PrelisFactory:
                     int(date.get('minutes')),
                 ).isocalendar()
             })
-        return recipient
+        return self.recipe
+
+    def build(self):
+        datas = self.readFile()
+        datas = self.extractDate()
+        return datas
 
 
 class LogFactory:
-    def readFile(self, filename: str) -> str:
+    def __init__(self, filename: str):
+        self._filename = filename
+        self._lines = None
+        self._recipe = None
+
+    @property
+    def filename(self):
+        return self._filename
+
+    @filename.setter
+    def filename(self, new_filename: str):
+        self._filename = new_filename
+
+    @property
+    def recipe(self):
+        return self._recipe
+
+    @recipe.setter
+    def recipe(self, new_recipe: List):
+        self._recipe = new_recipe
+
+    @property
+    def lines(self):
+        return self._lines
+
+    @lines.setter
+    def lines(self, new_lines: List):
+        self._lines = new_lines
+
+    def readFile(self):
         parent = pathlib.Path(__file__).parent
-        filepath = parent / f'{LOG_PATH_DIR}/{filename}'
-        datetime_code = datetime.now().strftime(format="%Y%m%d_%H_%m_%s")
-        output_file = os.path.join(parent, f'outputs/access_logs_{datetime_code}.parquet')
-        errors_file = os.path.join(parent, f'outputs/log_errors_{datetime_code}.csv')
-        adv.logs_to_df(
-            log_file=filepath,
-            output_file=output_file,
-            errors_file=errors_file,
-            log_format='common',
-            fields=None
-        )
-        logs_df = pd.read_parquet(
-            filename=output_file,
-            encoding='ISO-8859-1',
-            low_memory=False
-        )
+        filepath = parent / f'{LOG_PATH_DIR}/{self.filename}'
+        with open(filepath, encoding="Latin-1") as file:
+            string = file.read()
+            pattern = re.compile(r'\s+'.join(ANNULATION_REGEX))
+            rollbacks = re.findall(pattern, string)
+            pattern = re.compile(r'\s+'.join(VIREMENT_REGEX))
+            transfers = re.findall(pattern, string)
+            self.lines = rollbacks.extend(transfers)
+            file.close()
+        return self.lines
 
 
 def main():
-    instance = PrelisFactory()
-    datas = instance.readFile('PRELIS_21252')
-    recipient = instance.extractDate(datas)
-    print(recipient)
+    instance = LogFactory('midemv-2021-09-08.log')
+    datas = instance.readFile()
+    print(datas)
 
 
 if __name__ == '__main__':
-    instance = LogFactory()
-    datas = instance.readFile('midemv-2021-09-08.log')
-    print(datas)
+    main()
